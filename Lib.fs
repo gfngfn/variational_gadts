98ff6e2747335258b752046bf7d68f6b87c9cde9
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2021-01-31 01:44:20 +0900

    use 'FParsec' (2)

--- Lib.fs
+++ Lib.fs
@@ -1,5 +1,9 @@
 module Lib
 
+open FParsec.CharParsers
+
 // Define a function to construct a message to print
-let from whom =
-  sprintf "from %s" whom
+let from s =
+  match run pfloat s with
+  | Success(res, _, _) -> sprintf "success: %A" res
+  | Failure(msg, _, _) -> sprintf "failure: %s" msg
