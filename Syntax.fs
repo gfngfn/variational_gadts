6ede2429e7c313af2ef4532cb145e21c97b99e89
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2021-02-08 02:50:15 +0900

    cosmetic changes

--- Syntax.fs
+++ Syntax.fs
@@ -55,12 +55,12 @@ and AstMain =
 
   override this.ToString () =
     match this with
-    | Var(Ident(_, x))     -> sprintf "Var(\"%s\")" x
-    | Apply(e1, e2)        -> sprintf "Apply(%O, %O)" e1 e2
-    | Lambda(ident, e)     -> sprintf "Lambda(%O, %O)" ident e
-    | LetIn(i, e1, e2)     -> sprintf "LetIn(%O, %O, %O)" i e1 e2
-    | LetRecIn(i, e1, e2)  -> sprintf "LetRecIn(%O, %O, %O)" i e1 e2
-    | BaseConstant(bc)     -> sprintf "BaseConstant(%O)" bc
+    | Var(Ident(_, x))      -> sprintf "Var(\"%s\")" x
+    | Apply(e1, e2)         -> sprintf "Apply(%O, %O)" e1 e2
+    | Lambda(ident, e)      -> sprintf "Lambda(%O, %O)" ident e
+    | LetIn(i, e1, e2)      -> sprintf "LetIn(%O, %O, %O)" i e1 e2
+    | LetRecIn(i, e1, e2)   -> sprintf "LetRecIn(%O, %O, %O)" i e1 e2
+    | BaseConstant(bc)      -> sprintf "BaseConstant(%O)" bc
     | Constructor(ctor, es) -> sprintf "Constructor(%s, %O)" ctor es
 
 
