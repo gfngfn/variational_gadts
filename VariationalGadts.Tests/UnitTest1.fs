71a992613f8547648fc9f35571c3e9b6d9a15350
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2021-02-15 08:14:51 +0900

    develop a simple test

--- VariationalGadts.Tests/UnitTest1.fs
+++ VariationalGadts.Tests/UnitTest1.fs
@@ -2,10 +2,22 @@ module VariationalGadts.Tests
 
 open NUnit.Framework
 
+open MyUtil
+
 [<SetUp>]
 let Setup () =
     ()
 
 [<Test>]
 let Test1 () =
-    Assert.Pass()
+  let input = "let apply = fun x -> fun y -> x 0 (cons y []) in apply"
+  let tyenv = Primitives.initialTypeEnvironment
+  let res =
+    result {
+      let! e = Parser.parse input |> Result.mapError (fun _ -> ())
+      let! tyGot = TypeChecker.typecheck tyenv e |> Result.mapError (fun _ -> ())
+      return tyGot
+    }
+  match res with
+  | Ok(_)    -> Assert.Pass()
+  | Error(_) -> Assert.Fail()
