9007dd8943c510eb680b739b0aaa6e2046696fbc
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2021-02-16 00:59:21 +0900

    refactor tests

--- VariationalGadts.Tests/UnitTest1.fs
+++ VariationalGadts.Tests/UnitTest1.fs
@@ -8,27 +8,30 @@ open MyUtil
 let Setup () =
     ()
 
-[<TestCase(
-  "let apply = fun x -> fun y -> x 0 (cons y []) in apply"
-)>]
-[<TestCase(
-  """
-  let rec foldl = fun f -> fun acc -> fun xs ->
-    decompose_list xs
-      (fun u -> acc)
-      (fun y -> fun ys -> foldl f (f acc y) ys)
-  in
-  foldl
-  """
-)>]
-let Test1 (input : string) =
-  let tyenv = Primitives.initialTypeEnvironment
-  let res =
-    result {
-      let! e = Parser.parse input |> Result.mapError (fun _ -> ())
-      let! tyGot = TypeChecker.typecheck tyenv e |> Result.mapError (fun _ -> ())
-      return tyGot
-    }
-  match res with
-  | Ok(_)    -> Assert.Pass()
-  | Error(_) -> Assert.Fail()
+[<Test>]
+let Test1 () =
+  let check input =
+    let tyenv = Primitives.initialTypeEnvironment
+    let res =
+      result {
+        let! e = Parser.parse input |> Result.mapError (fun _ -> ())
+        let! tyGot = TypeChecker.typecheck tyenv e |> Result.mapError (fun _ -> ())
+        return tyGot
+      }
+    match res with
+    | Ok(_)    -> true
+    | Error(_) -> false
+
+  Assert.IsTrue(
+    check """
+      let apply = fun x -> fun y -> x 0 (cons y []) in apply
+    """)
+  Assert.IsTrue(
+    check """
+      let rec foldl = fun f -> fun acc -> fun xs ->
+        decompose_list xs
+          (fun u -> acc)
+          (fun y -> fun ys -> foldl f (f acc y) ys)
+      in
+      foldl
+    """)
