84e72314a66605754dde2c2b0041d64149df2576
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2021-02-20 20:14:20 +0900

    refactor 'Parser' by using 'token' and 'enclose'

--- VariationalGadts/Parser.fs
+++ VariationalGadts/Parser.fs
@@ -21,15 +21,23 @@ let withRange (p : Parser<'r, 'u>) : Parser<Range * 'r, 'u> =
   pipe3 getPosition p getPosition (fun posL v posR -> (ValidRange(posL, posR), v))
 
 
-let lowerParser : Parser<string, unit> =
+let token (s : string) : Parser<unit, unit> =
+  pstring s .>> spaces |>> fun _ -> ()
+
+
+let enclose (p : Parser<'a, unit>) : Parser<'a, unit> =
+  between (token "(") (token ")") p
+
+
+let lowerToken : Parser<string, unit> =
   let isIdentifierFirstChar c = isLower c
   let isIdentifierChar c = isLetter c || isDigit c || c = '_'
   many1Satisfy2L isIdentifierFirstChar isIdentifierChar "identifier"
 
 
-let identifierParser : Parser<Ident, unit> =
+let identifierToken : Parser<Ident, unit> =
   let pMain =
-    lowerParser >>= begin function
+    lowerToken >>= begin function
     | "val"
     | "fun"
     | "let" | "in" | "rec"
@@ -53,7 +61,7 @@ let constructorParser : Parser<Constructor, unit> =
 
 
 let typeVariableParser : Parser<ManualTypeVar, unit> =
-  pstring "'" >>. lowerParser
+  pstring "'" >>. lowerToken
 
 
 let integerParser : Parser<Range * int, unit> =
@@ -68,8 +76,8 @@ let rec absLevelParser : Parser<Ast, unit> =
 
 and letParser : Parser<Ast, unit> =
   fun s ->
-  let pValBind = pstring "let" .>> spaces >>. valueBindingParser
-  let pInner = pstring "in" .>> spaces >>. absLevelParser
+  let pValBind = token "let" >>. valueBindingParser
+  let pInner = token "in" >>. absLevelParser
   let p = pipe2 pValBind pInner (fun valbind e -> LocalBinding(valbind, e))
   (withRange p) s
 
@@ -78,10 +86,10 @@ and valueBindingParser : Parser<ValueBinding, unit> =
   fun s ->
   let p1 : Parser<bool * Ident, unit> =
     let pRec =
-       opt (pstring "rec" .>> spaces) |>> function
+       opt (token "rec") |>> function
        | None    -> false
        | Some(_) -> true
-    pRec .>>. identifierParser .>> spaces .>> pstring "=" .>> spaces
+    pRec .>>. identifierToken .>> spaces .>> token "="
   let p2f ((isRec, ident) : bool * Ident) : Parser<ValueBinding, unit> =
     absLevelParser |>> (fun e1 ->
       if isRec then
@@ -94,9 +102,9 @@ and valueBindingParser : Parser<ValueBinding, unit> =
 
 and ifParser : Parser<Ast, unit> =
   fun s ->
-  let p0 = pstring "if" .>> spaces >>. absLevelParser .>> spaces
-  let p1 = pstring "then" .>> spaces >>. absLevelParser .>> spaces
-  let p2 = pstring "else" .>> spaces >>. absLevelParser .>> spaces
+  let p0 = token "if" >>. absLevelParser
+  let p1 = token "then" >>. absLevelParser
+  let p2 = token "else" >>. absLevelParser
   let p = withRange (pipe3 p0 p1 p2 (fun e0 e1 e2 -> IfThenElse(e0, e1, e2)))
   p s
 
@@ -104,7 +112,7 @@ and ifParser : Parser<Ast, unit> =
 and abstractionParser : Parser<Ast, unit> =
   fun s ->
   let p1 : Parser<Ident, unit> =
-    pstring "fun" .>> spaces >>. identifierParser .>> spaces .>> pstring "->" .>> spaces
+    token "fun" >>. identifierToken .>> spaces .>> token "->"
   let p2f (ident : Ident) : Parser<AstMain, unit> =
     absLevelParser >>= fun e -> preturn (Lambda(ident, e))
   let p = withRange (p1 >>= p2f)
@@ -122,22 +130,22 @@ and appLevelParser : Parser<Ast, unit> =
 
 and bottomLevelParser : Parser<Ast, unit> =
   fun s ->
-  let pIdent = identifierParser >>= fun ident -> match ident with Ident(r, _) -> preturn (r, Var(ident))
+  let pIdent = identifierToken >>= fun ident -> match ident with Ident(r, _) -> preturn (r, Var(ident))
   let pCtorApp = constructorApplicationParser |>> fun (r, (ctor, es)) -> (r, Constructor(ctor, es))
   let pNil = (withRange (pstring "[]")) |>> fun (r, _) -> (r, Constructor(Ctor(r, "[]"), []))
   let pTrue = withRange (pstring "true" |>> fun _ -> BaseConstant(BooleanValue(true)))
   let pFalse = withRange (pstring "false" |>> fun _ -> BaseConstant(BooleanValue(false)))
   let pUnitConst = withRange (pstring "()" |>> fun _ -> BaseConstant(UnitValue))
   let pIntConst = integerParser |>> fun (r, n) -> (r, BaseConstant(IntegerValue(n)))
-  let pParen = between (pstring "(" .>> spaces) (pstring ")") absLevelParser
-  let p = ((pIdent <|> pNil <|> pTrue <|> pFalse <|> pUnitConst <|> pIntConst <|> pParen) .>> spaces) <|> pCtorApp
+  let pParen = enclose absLevelParser
+  let p = ((pIdent <|> pNil <|> pTrue <|> pFalse <|> pUnitConst <|> pIntConst) .>> spaces) <|> pParen <|> pCtorApp
   p s
 
 
 and constructorApplicationParser : Parser<Range * (Constructor * Ast list), unit> =
   fun s ->
   let pCtor = constructorParser .>> spaces
-  let pArg = between (pstring "(" .>> spaces) (pstring ")" .>> spaces) (many absLevelParser)
+  let pArg = enclose (many absLevelParser)
   let p = withRange (pCtor .>>. pArg)
   p s
 
@@ -151,14 +159,14 @@ let rec typeParser : Parser<ManualType, unit> =
 and arrowLevelTypeParser : Parser<ManualType, unit> =
   fun s ->
   let pMain =
-    appLevelTypeParser .>> (pstring "->" .>> spaces) .>>. arrowLevelTypeParser
+    appLevelTypeParser .>> (token "->") .>>. arrowLevelTypeParser
   let p = withRange (pMain |>> fun (mnty1, mnty2) -> MFuncType(mnty1, mnty2))
   p s
 
 
 and appLevelTypeParser : Parser<ManualType, unit> =
   fun s ->
-  let p1 = identifierParser |>> function Ident(_, tyident) -> tyident
+  let p1 = identifierToken |>> function Ident(_, tyident) -> tyident
   let p2 = many1 bottomLevelTypeParser
   let p = withRange (pipe2 p1 p2 (fun tyident mntys -> MDataType(tyident, mntys)))
   p s
@@ -167,31 +175,31 @@ and appLevelTypeParser : Parser<ManualType, unit> =
 and bottomLevelTypeParser : Parser<ManualType, unit> =
   fun s ->
   let pTyVar = withRange (typeVariableParser |>> fun s -> MTypeVar(s))
-  let pTy = withRange (identifierParser |>> function Ident(_, tyident) -> MDataType(tyident, []))
-  let pParen = between (pstring "(" .>> spaces) (pstring ")") typeParser
-  let p = (pTyVar <|> pTy <|> pParen) .>> spaces
+  let pNullary = withRange (identifierToken |>> function Ident(_, tyident) -> MDataType(tyident, []))
+  let pParen = enclose typeParser
+  let p = ((pTyVar <|> pNullary) .>> spaces) <|> pParen
   p s
 
 
 let generalizedConstructorBranchParser : Parser<GeneralizedConstructorBranch, unit> =
-  let pBar = pstring "|" .>> spaces
+  let pBar = token "|"
   let pCtor = constructorParser .>> spaces
   let pTyVars = many (typeVariableParser .>> spaces)
-  let pParamTys = between (pstring "(" .>> spaces) (pstring ")" .>> spaces) (many typeParser)
+  let pParamTys = sepBy typeParser (token ",")
   pipe3 (pBar >>. pCtor) pTyVars pParamTys (fun ctor tyvars mntys -> GeneralizedConstructorBranch(ctor, tyvars, mntys))
 
 
 let typeBindingParser : Parser<TypeBinding, unit> =
   fun s ->
-  let pIdent = (identifierParser .>> spaces) |>> function Ident(_, tyident) -> tyident
-  let pBranches = pstring "=" .>> spaces >>. many generalizedConstructorBranchParser
+  let pIdent = (identifierToken .>> spaces) |>> function Ident(_, tyident) -> tyident
+  let pBranches = token "=" >>. many generalizedConstructorBranchParser
   let p = pipe3 pIdent integerParser pBranches (fun tyident (_, arity) gctorbrs -> Generalized(tyident, arity, gctorbrs))
   p s
 
 
 let bindingParser : Parser<Binding, unit> =
-  let pValBind = pstring "val" .>> spaces >>. valueBindingParser |>> (fun valbind -> BindValue(valbind))
-  let pTyBind = pstring "type" .>> spaces >>. typeBindingParser |>> (fun tybind -> BindType(tybind))
+  let pValBind = token "val" >>. valueBindingParser |>> (fun valbind -> BindValue(valbind))
+  let pTyBind = token "type" >>. typeBindingParser |>> (fun tybind -> BindType(tybind))
   pValBind <|> pTyBind
 
 
